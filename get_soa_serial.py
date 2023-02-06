a453299a860a13ec998989ad8dc40a1b0a79fa7d
Chris Appleyard <github@chrisappleyard.net>
2023-02-06 21:09:43 +0000

    Scripts to be used for zone age for monitoring

---
+++ get_soa_serial.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import argparse
+import dns.resolver
+
+def get_soa_serial(nameserver, zone):
+    resolver = dns.resolver.Resolver()
+    resolver.nameservers = [nameserver]
+    try:
+        answer = resolver.resolve(zone, 'SOA')
+        serial_number = answer.rrset[0].serial
+        return str(serial_number)
+    except dns.resolver.Timeout:
+        try:
+            resolver.use_tcp = True
+            answer = resolver.resolve(zone, 'SOA')
+            serial_number = answer.rrset[0].serial
+            return str(serial_number)
+        except dns.resolver.Timeout:
+            print("Failed to get SOA serial for " + zone + " , query timed out")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("zone")
+args = parser.parse_args()
+zone = args.zone
+
+serial = get_soa_serial('1.1.1.1', zone)
+print(serial)
---
+++ get_soa_serial.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+zone="$1"
+
+function get_soa_serial() {
+  local nameserver="$1"
+  local zone="$2"
+  local serial_number
+
+  serial_number=$(dig @"$nameserver" "$zone" SOA +short | awk '{print $3}')
+  if [[ -z "$serial_number" ]]; then
+    serial_number=$(dig +tcp @"$nameserver" "$zone" SOA +short | awk '{print $3}')
+  fi
+
+  if [[ -z "$serial_number" ]]; then
+    echo "Failed to get SOA serial for $zone, query timed out" >&2
+    return 1
+  fi
+
+  echo "$serial_number"
+}
+
+serial=$(get_soa_serial "1.1.1.1" "$zone")
+echo "$serial"
